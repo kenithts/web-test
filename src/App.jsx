aba73c4ddebe16b2a9d2770098a3fed3e245c15d
Kenith Suga <kenithts6@gmail.com>
2020-11-07 13:38:34 -0300

    [SETUP] Added ReactRouter and PropTypes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import Authenticated from './Layouts/Authenticated';
 
 function App() {
   return (
-    <div>
-      App
-    </div>
+    <Router>
+      <Switch>
+        <Authenticated>
+          <Route path="/dashboard/quick-ops/current" />
+        </Authenticated>
+      </Switch>
+    </Router>
   );
 }
 
