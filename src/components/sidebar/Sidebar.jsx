19afdf06c5eb582e8cf48b8c8ed65186df058269
Kenith Suga <kenithts6@gmail.com>
2020-11-07 16:38:21 -0300

    [SETUP] Setting up internationalization

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import './styles.scss';
 
-const Sidebar = () => (
-  <div className="sidebar__wrapper" />
-);
+const Sidebar = () => {
+  const { t } = useTranslation('pages');
+  return (
+    <div className="sidebar__wrapper">
+      {t('current')}
+    </div>
+  );
+};
 
 export default Sidebar;
